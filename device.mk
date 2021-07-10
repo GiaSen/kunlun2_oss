5625a48a0a596b1bbd72a2835d783cc56ef8301c
bgcngm <bgcngm@gmail.com>
2021-07-10 16:59:55 +0200

    Add whitelist to support exposing vendor libs
    From ZUI 11.5.160

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(LOCAL_PATH)/configs/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
+# Native libraries whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
