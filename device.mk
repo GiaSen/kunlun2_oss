11d6885d9387ba11af7da7effbac5a345721a612
Giammarco Senatore <giammasena@gmail.com>
2021-07-15 00:41:10 +0200

    Drop android.hardware.thermal HAL

--- device.mk
+++ device.mk
@@ -343,14 +343,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
-# Thermal HAL
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
