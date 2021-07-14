835641f2cfa28773bb89d9fd12efdcb5b5ed288b
Giammarco Senatore <giammasena@gmail.com>
2021-07-15 01:26:57 +0200

    kunlun2: drop livedisplay
    This is eleven branch, not plus
    * Yeah I am debugging on Plus idk why

--- device.mk
+++ device.mk
@@ -182,12 +182,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.lenovo_kunlun2
 
-# Livedisplay
-ifeq ($(findstring Plus, $(CUSTOM_VERSION)),)
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-endif
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
