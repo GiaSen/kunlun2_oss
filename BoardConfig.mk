aexmod-tmp/platform_device_oneplus_enchilada

Move DPM feature property to /system_ext
 * DPM blobs are under /system_ext.

Change-Id: I6bafdc3b310168c6b4d681c9eb94eb4e2e43c5e5
Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
Signed-off-by: DennySPb <dennyspb@gmail.com>

BoardConfig.mk
@@ -95,6 +95,7 @@ BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 TARGET_ODM_PROP += $(VENDOR_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(VENDOR_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(VENDOR_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(VENDOR_PATH)/vendor.prop
 
 # Telephony

GiaSen/kunlun2_oss

BoardConfig.mk
@@ -104,6 +104,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Root